c02d0389e74b1de7909b77e65c94cc546bc0cb8c
alexandereka <alexander_gunawan@ekahospital.com>
2023-06-19 16:25:57 +0700

    First commit
    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KCompanyWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
